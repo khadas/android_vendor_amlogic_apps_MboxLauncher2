d693355b008b69b9df401d8cb6704932c78a689c
Tellen Yu <tellen.yu@amlogic.com>
2018-11-02 15:23:12 +0800

    consumerir: add consumerir for P [4/4]
    PD#SWPL-1163
    
    Problem:
    consumerir hal has not been added in P
    
    Solution:
    add consumerir hal in P
    
    Verify:
    verify on franklin
    
    Change-Id: I44599ba1dbbe4c2a81d2f809eaef71a493e67089
    Signed-off-by: Tellen Yu <tellen.yu@amlogic.com>

--- Android.mk
+++ Android.mk
@@ -9,6 +9,14 @@ LOCAL_PACKAGE_NAME := SmartRemote
 
 #LOCAL_SDK_VERSION := current
 
+ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 26 && echo OK),OK)
+LOCAL_PROPRIETARY_MODULE := true
+endif
+
+ifndef PRODUCT_SHIPPING_API_LEVEL
+LOCAL_PRIVATE_PLATFORM_APIS := true
+endif
+
 include $(BUILD_PACKAGE)
 
 # Use the following include to make our test apk.
